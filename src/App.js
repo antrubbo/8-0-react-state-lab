b723f07d48508c1008e59709298c6dc3dea04014
Anthony Rubbo <anthony.rubbo@gmail.com>
2021-11-02 18:06:58 -0400

    completed, tests unclear/not passing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,49 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      currentScore: 0,
+      incrementor: 1
+    }
+  }
+
+  handleIncreaseScore = () => {
+    this.setState({
+      currentScore: this.state.currentScore + this.state.incrementor
+    })
+  }
+
+  handleIncreaseIncrementor = () => {
+    if(this.state.currentScore >= 10){
+      this.setState({
+        currentScore: this.state.currentScore - 10,
+        incrementor: this.state.incrementor + 1
+      })
+    } else {
+      alert("You can't afford that!")
+    }
+  }
+
+  handlePlayAgain = () => {
+    this.setState({
+      currentScore: 0,
+      incrementor: 1
+    })
+  }
+
   render() {
+    const {currentScore, incrementor} = this.state
+
     return (
       <main>
-        <p>React State Lab</p>
+        <h2>Current Score: {currentScore}</h2>
+        <button style={currentScore >= 100 ? {display: "none"} : null} onClick={this.handleIncreaseScore}>+{incrementor}</button>
+        <br /><br />
+        <button style={currentScore >= 100 ? {display: "none"} : null} onClick={this.handleIncreaseIncrementor}>Pay 10 points to change from +{incrementor} to +{incrementor + 1}</button>
+        <h2 style={currentScore <= 99 ? {display: "none"} : null}>You Win!</h2>
+        <button style={currentScore <= 99 ? {display: "none"} : null} onClick={this.handlePlayAgain}>Play Again?</button>
       </main>
     );
   }
